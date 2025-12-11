fbaaf27cb87838ce0bb2b01d72220f7269de5dd1
AlumnoAfternoon <sergiodamsegundo@gmail.com>
2025-12-11 21:25:47 +0100

    11/12/2025 - ftp

--- src/edu/thepower/u4serviciosEnRed/U4P03SimpleWebServer.java
+++ src/edu/thepower/u4serviciosEnRed/U4P03SimpleWebServer.java
@@ -6,55 +6,99 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class U4P03SimpleWebServer {
-    private static final String HTML_1 = """
+
+    private static final String HTML = """
             <html>
                 <head>
-                    <title>Simple Web Server</title>
+                    <title>Servidor web simple</title>
                 </head>
                 <body>
-                    <h1>Hola mundo</h1>
-                    <p>Visitante número[""";
-    private static final String HTML_2 = """
-                    ]</p>
+                    <h1>¡Hola mundo!</h1>
+                    <p>Esto es un servidor web simple.</p>
+                    <p>%s</p>
                 </body>
-            </html>
-            """;
-    private static final int PORT = 2100;
-    private static AtomicInteger contador = new AtomicInteger(0);
+            </html>""";
 
-    private static void atenderSolcitud(Socket socket) {
+    private static final int PUERTO = 8080;
+    private static final String NOMBRE_SERVIDOR = "Servidor Web Simple";
+    private static AtomicInteger contador = new AtomicInteger();
+    private static final String HTTP_ESTADO_OKAY = "200 OK";
+    private static final String HTTP_ESTADO_NOT_FOUND = "404 Not Found";
+    private static final String HTTP_ESTADO_NOT_ALLOWED = "405 Method Not Allowed";
+    private static final DateTimeFormatter FECHA_ACTUAL = DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm:ss");
+
+    public static void main(String[] args) {
+        try (ServerSocket svs = new ServerSocket(PUERTO)) {
+            System.out.println("Servidor escuchando en el puerto " + PUERTO);
+            while (true) {
+                Socket socket = svs.accept();
+                Thread t = new Thread(() -> atenderSolicitud(socket));
+                t.start();
+            }
+        } catch (IOException e) {
+            System.err.println("Error en el servidor: " + e.getMessage());
+        }
+    }
+
+    private static void atenderSolicitud(Socket socket) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             PrintWriter pw = new PrintWriter(socket.getOutputStream());) {
-            String linea = br.readLine();
-            System.out.println(linea);
-            System.out.println("Devolviendo HTML...");
-            StringBuffer respuesta = new StringBuffer();
-            respuesta.append(HTML_1).append(contador.get()).append(HTML_2);
-            pw.println("HTTP/1.1 200 OK");
-            pw.println("Content-Type:text/html;charset=UTF-8");
-            pw.println("Content-Length: " + respuesta.toString().getBytes().length);
-            pw.println();
-            pw.println(respuesta.toString());
-            pw.flush();
+             PrintWriter pw = new PrintWriter(socket.getOutputStream())) {
+            String linea;
+            linea = br.readLine();
+            if (linea != null && !linea.isBlank()) {
+                System.out.println(linea);
+                String[] partes = linea.split("\\s");
+                String metodo = partes[0];
+                String ruta = partes.length > 1 ? partes[1].trim().toLowerCase() : "/";
+
+                while ((linea = br.readLine()) != null && !linea.isBlank()) {
+                    System.out.println(linea);
+                }
+                String estado = HTTP_ESTADO_OKAY;
+                String respuesta = "";
+                if (metodo.trim().equalsIgnoreCase("get")) {
+                    // Desechar solicitudes GET del favicon
+                    if (ruta.contains("favicon")) {
+                        contador.incrementAndGet(); // Solo contabilizamos la visita si es de tipo GET y la ruta no incluye favicon
+                    }
+                    System.out.println("Devolviendo respuesta HTML: ");
+
+                    estado = HTTP_ESTADO_OKAY;
+                    respuesta = switch (ruta) {
+                        case "/" -> "Eres el visitante número: " + contador.incrementAndGet();
+                        case "/fecha", "/fecha/" ->
+                                "Fecha y hora actuales: " + FECHA_ACTUAL.format(LocalDateTime.now());
+                        case "/nombre", "/nombre/" -> "El nombre del servidor es: " + NOMBRE_SERVIDOR;
+                        default -> {
+                            estado = HTTP_ESTADO_NOT_FOUND;
+                            yield "Error: ruta no permitida";
+                        }
+                    };
+                } else {
+                    estado = HTTP_ESTADO_NOT_ALLOWED;
+                    respuesta = "Error: método no permitido";
+                }
+                devolverRespuesta(pw, estado, respuesta);
+            }
         } catch (IOException e) {
             System.err.println("Error en el servidor " + e.getMessage());
         }
     }
-    public static void main(String[] args) {
-        try(ServerSocket sc = new ServerSocket(PORT)) {
-            System.out.println("Escuchando en el puerto " + PORT);
-            while(true) {
-                Socket socket = sc.accept();
-                // Incrementamos el número de visitas en uno
-                contador.incrementAndGet();
-                Thread t = new Thread(() -> atenderSolcitud(socket));
-                t.start();
-            }
-        } catch(IOException e) {
-            throw new RuntimeException(e);
-        }
+
+    private static void devolverRespuesta(PrintWriter pw, String estado, String mensaje) {
+        StringBuffer respuesta = new StringBuffer();
+        respuesta.append(String.format(HTML, mensaje));
+        pw.println("HTTP/1.1 " + estado);
+        pw.println("Content-Type: text/html;charset=UTF-8");
+        pw.println("Content-Length: " + respuesta.toString().getBytes().length);
+        pw.println();
+        pw.println(respuesta.toString());
+
+        pw.flush();
     }
 }
---
+++ src/edu/thepower/u4serviciosEnRed/U4P04FtpClient.java
@@ -0,0 +1,53 @@
+package edu.thepower.u4serviciosEnRed;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+
+import java.io.*;
+
+public class U4P04FtpClient {
+    public static void main(String[] args) {
+        FTPClient ftp = new FTPClient();
+        // 1. Conectarse al servidor FTP
+        try {
+            ftp.connect("eu-central-1.sftpcloud.io"); // Cómo el puerto por defecto es 21 y nos vamos a conectar al 21, no hace falta ponerlo como argumento
+            System.out.println("Conectado al servidor FTP");
+            // 2. Loggearse
+            if(ftp.login("990fa04831eb473b9cf4ebe4eace3ab1", "oL91IvJKCW0rjMYGgWwXtaeNqEvne27F")) {
+                System.out.println("Acceso correcto al servidor FTP");
+                // 3. Activar modo pasivo (cliente siempre se conecta al servidor, no al revés), los datos enviados en binario
+                ftp.enterLocalPassiveMode();
+                ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+
+                // 4. Enviar un archivo
+                InputStream is = new FileInputStream("C:\\Users\\AlumnoAfternoon\\Desktop\\PSP\\resources\\vocales.txt");
+                String ficheroRemoto = "archivo_remoto.txt";
+
+                if(ftp.storeFile(ficheroRemoto, is)) {
+                    System.out.println("Fichero subido correctamente al servidor FTP");
+                } else {
+                    System.err.println("Error en la subida del archivo al servidor FTP");
+                }
+
+                // 5. Listado del contenido del servidor FTP
+                FTPFile[] lista = ftp.listFiles();
+                for(FTPFile f : lista) {
+                    System.out.println(f.getName());
+                }
+
+                // 6. Descargar el archivo del servidor FTP
+                OutputStream os = new FileOutputStream("C:\\Users\\AlumnoAfternoon\\Desktop\\PSP\\resources\\archivo_descargado_desde_ftp");
+                if(ftp.retrieveFile(ficheroRemoto, os)) {
+                    System.out.println("Fichero descargado correctamente del servidor FTP");
+                } else {
+                    System.err.println("Error en la descarga del archivo del servidor FTP");
+                }
+            } else {
+                System.err.println("Error de acceso al servidor FTP");
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
